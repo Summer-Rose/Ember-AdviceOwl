96f1475e48d3cfe28360b76a7ad09b20ceae30c7
Summer <sbrochtrup@gmail.com>
2015-10-02 14:41:28 -0700

    Collect date input

--- app/components/add-answer.js
+++ app/components/add-answer.js
@@ -7,6 +7,7 @@ export default Ember.Component.extend({
         username: this.get('username') ? this.get('username') : "",
         answer: this.get('answer') ? this.get('answer') : "",
         question: this.get('question') ? this.get('question') : "",
+        date: this.get('date') ? this.get('date') : "",
       };
       this.sendAction('saveAnswer', params);
     }
