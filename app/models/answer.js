9ccd03cb769e280019883a2cc3bcb05755ffc496
Summer <sbrochtrup@gmail.com>
2015-10-02 09:05:40 -0700

    Add category and date fields to answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   username: DS.attr(),
+  date: DS.attr(),
   answer: DS.attr(),
   question: DS.belongsTo('question', { async: true}),
 });
