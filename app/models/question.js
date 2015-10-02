b81afd20fc38e63b45e0be31d0015642f959f062
Summer <sbrochtrup@gmail.com>
2015-10-02 09:06:19 -0700

    Add category and date fields to question model

--- app/models/question.js
+++ app/models/question.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   username: DS.attr(),
+  date: DS.attr(),
+  category: DS.attr(),
   question: DS.attr(),
   answers: DS.hasMany('answer', { async: true})
 });
