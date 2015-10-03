b1512b2c2c92b6eeb7ef6ce376fb2eb2475c2551
Summer <sbrochtrup@gmail.com>
2015-10-02 17:03:19 -0700

    Update question to questionTitle

--- app/components/add-question.js
+++ app/components/add-question.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
     saveQuestion() {
       var params = {
         username: this.get('username') ? this.get('username') : "",
-        question: this.get('question') ? this.get('question') : "",
+        questionTitle: this.get('question') ? this.get('question') : "",
         category: this.get('category') ? this.get('category') : "",
         date: this.get('date') ? this.get('date') : "",
       };
